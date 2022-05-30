641d7549d8d33b69c3e81c294dc35bae9f9762a7
d0vgan <dvv81@ukr.net>
2022-05-30 20:52:38 +0300

    adding .bat files for command-line building

---
+++ AkelTags_build_vc2017_win32.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set VC_ROOT=%ProgramFiles(x86)%\Microsoft Visual Studio\2017
+
+if exist "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcProfessional
+if exist "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcCommunity
+
+goto ErrorNoVcVarsAll
+
+:UseVcProfessional
+call "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86
+goto Building
+
+:UseVcCommunity
+call "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+goto Building
+
+:Building
+msbuild AkelTags_VC2017.sln /t:Clean;Build /p:Configuration=Release;Platform=Win32 /m:2
+goto End
+
+:ErrorNoVcVarsAll
+echo ERROR: Could not find "vcvarsall.bat"
+goto End
+
+:End
